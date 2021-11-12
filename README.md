1981525b85edd9fb98b3d246399ece2651bf06ea
ryowuandjanet <ryowu0329@gmail.com>
2021-11-13 01:32:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Vue 3 + Vite
+
+This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+
+## 20211112新增內容
+- Vite 
+- Tailwind CSS
+- Vue Router
+- Vuex
+- ESLint
+- Prettier
+
+>### 執行
+npm run dev
+http://localhost:3000
+>### 編譯
+npm run build
+serve -s dist\
+http://localhost:5000
---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mb-6 text-3xl font-extrabold">Home</div>
+  <p class="mb-6">Name is store is: {{ name }}</p>
+  <input type="text" v-model="newName" class="p-2 mr-4 border border-gray-600 rounded" />
+  <button @click="saveName" class="p-2 text-white bg-indigo-600 rounded">Submit</button>
+</template>
+
+<script setup>
+  import { computed, ref } from 'vue'
+  import { useStore } from 'vuex'
+
+  const store = useStore()
+
+  import { useRouter } from 'vue-router'
+  const router = useRouter()
+
+  const name = computed(() => {
+    return store.state.user.name
+  })
+
+  const newName = ref('')
+  function saveName() {
+    store.dispatch('saveName', newName.value)
+    newName.value = ''
+    router.push('/about')
+  }
+</script>
