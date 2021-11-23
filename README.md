4a7e79e07b60b3e76159bf8c5205e16f13775508
ryowuandjanet <ryowu0329@gmail.com>
2021-11-23 21:19:05 +0800

    20211123 設定 /src 等於 @

--- README.md
+++ README.md
@@ -26,5 +26,5 @@ This template should help get you started developing with Vue 3 in Vite. The tem
 >先修改 netlify.tom 裡的 functions = "[專案名稱]/"
 >npm i netlify-cli -g<br>
 >netlify login<br>
->netlify deploy --prod
+>netlify deploy --prod<br>
 **或是使用 npm run netlify 來編譯並上傳到 Netlify**
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,11 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
+  },
+  // 設定 /src 等於 @
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    }
   }
 })
